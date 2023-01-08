904c6830fdbee04ebdb2cc985fa5c9eb22fb0d28
Napoleon-Christos Oikonomou <napoleonoikon@gmail.com>
2023-01-08 13:05:30 +0200

    bugfix

--- src/routes/call-by-meaning-routes.js
+++ src/routes/call-by-meaning-routes.js
@@ -67,8 +67,7 @@ router.post("/call", (req, res) => {
 		return res.status(400).send("Input parameters must have the same length.");
 	}
 
-	return request.post({
-		uri: "/gbm/search/",
+	return request.post(`${req.protocol}://${req.get("host")}/gbm/search`, {
 		form: { inputConcepts, outputConcepts },
 	}, (err, response, body) => {
 		if (err) console.error(err);
